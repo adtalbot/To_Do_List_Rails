d9697758ef01a8c337f29f1ce2ab2ac29a938789
audrie <audrie.talbot@gmail.com>
2015-08-27 11:56:51 -0700

    Passes task specs

--- db/migrate/20150827180558_create_lists.rb
+++ db/migrate/20150827180558_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
       t.column(:name, :string)
+      t.column(:description, :string)
 
       t.timestamps
     end
